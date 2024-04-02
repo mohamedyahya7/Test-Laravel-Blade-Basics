4fc6c1f3b114c9ff00c62029d051d4c691382701
mohamedyahya7 <79977814+mohamedyahya7@users.noreply.github.com>
2024-04-02 22:52:29 +0300

    Update rows.blade.php

--- resources/views/rows.blade.php
+++ resources/views/rows.blade.php
@@ -22,11 +22,19 @@
                             @foreach ($users as $user)
                                 {{-- Task: only every second row should have "bg-red-100" --}}
                             
-                                <tr @if($loop->even)class="bg-red-100"@endif>
+                                @if($loop->even)
+                                    <tr class="bg-red-100">
+                                        @else
+                                    <tr >
+                                        @endif
                                     <td>{{$loop->iteration}}</td>
                                     <td>{{ $user->name }}</td>
                                     {{-- Task: only the FIRST row should have email with "font-bold" --}}
-                                    <td @if($loop->first)class="font-bold"@endif>{{ $user->email }}</td>
+                                   @if($loop->first) <td class="font-bold">
+                                       @else
+                                       <td>
+                                       @endif
+                                        {{ $user->email }}</td>
                                     <td>{{ $user->created_at }}</td>
                                 </tr>
                             @endforeach
